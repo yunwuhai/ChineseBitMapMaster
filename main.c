78c83393cc37f3ef1efb5fc7aa8abf771e9db8ce
云雾海 <872712633@qq.com>
2021-04-24 18:39:25 +0800

    基本完成

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+  
+int main(void)  
+{  
+    FILE *fp = NULL;  
+    char ch = 0;
+
+    fp=fopen("gb2312.txt","wb");
+    if(fp==NULL)  
+    {  
+        perror("Cann't open gb2312.txt");  
+        return -1;  
+    }  
+    else
+    {
+        printf("Creat file gb2312.txt Success\n");
+    }
+
+    for (int i = 0xB0; i <= 0xF7; ++i)
+    {
+        for (int j = 0xA1; j<= 0xFE; ++j) 
+        {
+            printf("%c%c", i, j);
+            fputc(i, fp);
+            fputc(j, fp);
+        }
+    }
+
+    fclose(fp);  
+    printf("\nDone");  
+  
+    return 0;  
+} 
